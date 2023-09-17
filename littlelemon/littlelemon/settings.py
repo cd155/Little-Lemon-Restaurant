6e61ce6527d9ec7262158f1056fe1d33167dca6f
cd155 <cd155@163.com>
2023-09-17 15:21:11 -0400

    add static files dir

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
     "restaurant/static",
 ]
 
+# Static file location for production
+STATIC_ROOT = "/var/www/softifacts.com/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
